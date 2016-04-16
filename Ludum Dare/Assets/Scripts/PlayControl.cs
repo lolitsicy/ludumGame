7b08fe542398db41358846516cb27c14f49aa5f2
PastaTime <arran94@gmail.com>
2016-04-17 01:06:00 +1200

    Started work on PlayTest2 (For the linear movement model)

---
+++ Ludum Dare/Assets/Scripts/PlayControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayControl : MonoBehaviour {
+	public KeyCode moveRightKey = KeyCode.J;
+	public KeyCode moveLeftKey = KeyCode.L;
+	public KeyCode jumpKey = KeyCode.K;
+
+	public GameObject obj;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void moveRight() {
+
+	}
+
+	void moveLeft() {
+
+	}
+
+	void jump() {
+	}
+
+}
+
