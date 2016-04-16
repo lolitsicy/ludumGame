55db6548fec3e0384101eeb7d80be53bac18326f
Ayas <ayas1996@gmail.com>
2016-04-17 10:15:28 +1200

    some more changes with movement

--- Ludum Dare/Assets/Scripts/PlayerControler.cs
+++ Ludum Dare/Assets/Scripts/PlayerControler.cs
@@ -1,38 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerControler : MonoBehaviour {
-	public KeyCode moveRightKey = KeyCode.J;
-	public KeyCode moveLeftKey = KeyCode.L;
-	public KeyCode jumpKey = KeyCode.K;
+public class PlayerControler : MonoBehaviour
+{
 	public float jumpForce = 10f;
-	public float moveForce = 50f;
 	public Transform groundCheck;
 	private bool grounded = true;
-	private int jumpCount = 0;
 	private Rigidbody2D bodyBox;
-	public float moveSpeed = 10f;
-	public float turnSpeed = 50f;
 	public GameObject obj;
+	[HideInInspector] public float angle;
+	[HideInInspector] public bool facingRight = true;
+	[HideInInspector] public float x, y, chargeTime, avgAngle;
+	[HideInInspector] public Vector2 forceMagNormalize;
 
 
-	[HideInInspector] public float x, y;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		bodyBox = gameObject.GetComponent<Rigidbody2D> ();
-		bodyBox.freezeRotation = true;
+		angle = obj.GetComponent<Transform> ().eulerAngles.magnitude;
+		avgAngle = angle;
 	}
-	
+		
 	// Update is called once per frame
-	void Update () {
-		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
-		float angle = obj.GetComponent<Transform>().eulerAngles.magnitude;
-		x = jumpForce* Mathf.Cos (angle*Mathf.Deg2Rad);
-		y = jumpForce * Mathf.Sin (angle*Mathf.Deg2Rad);
-
-		if (grounded) {
-			jumpCount = 0;
-		}
+	void Update ()
+	{
+		grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
+		angle = obj.GetComponent<Transform> ().eulerAngles.magnitude;
+		x = jumpForce * Mathf.Cos (angle * Mathf.Deg2Rad);
+		y = jumpForce * Mathf.Sin (angle * Mathf.Deg2Rad);
 
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			moveLeft ();
@@ -42,27 +38,55 @@ public class PlayerControler : MonoBehaviour {
 			moveRight ();
 		}
 		if (Input.GetKey (KeyCode.UpArrow)) {
-			jump ();	
+				jump ();
+			
+		}
+
+	}
+
+	void moveRight ()
+	{
+		if ((angle > 0 && angle < 180)) {
+			if(angle == 0){
+				obj.GetComponent<Transform> ().Rotate (new Vector3 (0f, 0f, 1f));
+			}else{
+				obj.GetComponent<Transform> ().Rotate (new Vector3 (0f, 0f, -1f));
+
+			}
+
 		}
 	}
 
-	void moveRight() {
-		obj.GetComponent<Transform> ().Rotate (new Vector3 (0f,0f,-10f));
+	void moveLeft ()
+	{
+		if (angle < 180) {
+			
+			obj.GetComponent<Transform> ().Rotate (new Vector3 (0f, 0f, 1f));
+		}
 	}
 
-	void moveLeft() {
-		obj.GetComponent<Transform> ().Rotate (new Vector3 (0f,0f,10f));
-	}
-
-	void jump() {
-		jumpCount++;
-		//bodyBox.velocity.Set (bodyBox.velocity.x + x, bodyBox.velocity.y + y);
-//		bodyBox.velocity.y += y;
-		bodyBox.AddForce (new Vector2 (x, y));
-
+	void jump ()
+	{
+		bodyBox.AddForce (new Vector2(x, y));
 
 	}
-		
+
+	void FixedUpdate ()
+	{
+
+
+			
+	}
+
+	void Flip ()
+	{
+			
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+
+	}
 
 
 
