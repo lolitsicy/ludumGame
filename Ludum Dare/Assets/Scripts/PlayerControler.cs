add392189d73e59787cce6f166ffe1bc81370535
PastaTime <arran94@gmail.com>
2016-04-17 23:10:38 +1200

    Started on GameScreen (no spaces). Made modular wall prefabs. Control + L activates auto snapping now

--- Ludum Dare/Assets/Scripts/PlayerControler.cs
+++ Ludum Dare/Assets/Scripts/PlayerControler.cs
@@ -5,25 +5,20 @@ public class PlayerControler : MonoBehaviour {
 	
 	public KeyCode moveRightKey = KeyCode.RightArrow;
 	public KeyCode moveLeftKey = KeyCode.LeftArrow;
-	public KeyCode jumpKey = KeyCode.UpArrow;
-	public float jumpForce = 10f;
+	public KeyCode jumpKey = KeyCode.Space;
 	/** The number of degrees a players head will rotate per second */
-	public float turnSpeed = 10f;
+	public float turnSpeed = 4f;
 	public Transform groundCheck;
+	public GameObject obj;
 	private bool grounded = true;
 	private Rigidbody2D bodyBox;
 	private Transform viewPoint;
-	public GameObject obj;
-	[HideInInspector] public float angle;
-	[HideInInspector] public bool facingRight = true;
 	[HideInInspector] public float x, y, jumpMag, jumpCount = 0;
-	[HideInInspector] public Vector2 forceMagNormalize;
+	/** Time in seconds for the charge jump to be fully charged */
 	public float fullChargeJumpTime = 1f;
 	public float fullChargeJumpForce = 15000f;
 	public float minChargeJumpForce = 6000f;
 	private float chargeTime = 0f;
-	public float aerialDriftForce = 10f;
-	public float driftGravity = 0.5f;
 	private int score = 0;
 
 	/** 
@@ -43,15 +38,13 @@ public class PlayerControler : MonoBehaviour {
 		score = 0;
 		bodyBox = gameObject.GetComponent<Rigidbody2D> ();
 		viewPoint = obj.GetComponent<Transform> ();
-		angle = viewPoint.eulerAngles.z;
-
 	}
 		
 	// Update is called once per frame
 	void Update ()
 	{
 		grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
-		angle = viewPoint.eulerAngles.magnitude;
+		float angle = viewPoint.eulerAngles.z;
 		x = Mathf.Cos (angle * Mathf.Deg2Rad);
 		y = Mathf.Sin (angle * Mathf.Deg2Rad);
 
@@ -74,15 +67,6 @@ public class PlayerControler : MonoBehaviour {
 
 			jumpCount = 0;
 		} else {
-			moveInAir ();
-			if (Input.GetKey (moveLeftKey)) {
-				moveLeft ();
-			}
-
-			if (Input.GetKey (moveRightKey)) {
-				Debug.Log ("MoveRight");
-				moveRight ();
-			}
 			if (Input.GetKey (jumpKey)) {
 				Debug.Log ("Charging JUMP");
 				chargeJump ();
@@ -104,43 +88,13 @@ public class PlayerControler : MonoBehaviour {
 		}
 
 	}
-	/** Get rid of this perhaps */
-	void moveInAir() {
-		bodyBox.AddForce (getViewDirection () * aerialDriftForce);
-		bodyBox.gravityScale = driftGravity;
-	}
-
-	/**
-	 * Returns the current view direction based on user input keys
-	 * */
-	public Vector2 getViewDirection() {
-		float x = 0f;
-		float y = 0f;
-		if (Input.GetKey (moveLeftKey)) { 
-			x -= 1.0f; 
-		}
-		if (Input.GetKey (moveRightKey)) { 
-			x += 1.0f;
-		}
-		return new Vector2 (x, y);
-	}
 		
 	void moveRight() {
-		Debug.Log ("MoveRight: " + viewPoint.eulerAngles);
-		if (angle > 0 && angle <= 360) {
-			viewPoint.eulerAngles += turnSpeed *(new Vector3 (0f, 0f, -4f));
-		} else {
-			viewPoint.eulerAngles = new Vector3 (0f, 0f, 0f);
-		}
+		viewPoint.eulerAngles += turnSpeed *(new Vector3 (0f, 0f, -turnSpeed));
 	}
 
 	void moveLeft() {
-		Debug.Log ("MoveLeft: " + viewPoint.eulerAngles);
-		if (angle >= 0 && angle < 360) {
-			viewPoint.eulerAngles += turnSpeed *(new Vector3 (0f, 0f, 4f));
-		} else {
-			viewPoint.eulerAngles = new Vector3 (0f, 0f, -180f);
-		}
+		viewPoint.eulerAngles += turnSpeed *(new Vector3 (0f, 0f, turnSpeed));
 	}
 		
 
