ee46b126107b489c2604bb9faf3efdf96b225d12
PastaTime <arran94@gmail.com>
2016-04-18 22:49:40 +1200

    Major Feature: Sprite tiling completed.

---
+++ Ludum Dare/Assets/Scripts/Scale.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scale : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector3 scale = gameObject.GetComponent<Transform> ().lossyScale;
+		MeshRenderer meshRen = gameObject.GetComponent<MeshRenderer> ();
+		meshRen.material.mainTextureScale = new Vector2 (scale.x/2, scale.y/2);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
